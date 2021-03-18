6f86db5df821924160cd025308d92c8fa2d0542d
Jan H. Krüger <janhkrueger@outlook.com>
2021-03-18 20:17:48 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,20 @@ using namespace std;
     string convertIntToRoman(int integerValueToConvert) {
 
         /**
-         * Das hier ist das für diese Lösung kritische
+         * Das hier ist das für diese Lösung kritische Sache.
+         * Die Sonderfälle, ein Step vor den nächstgrößeren.
+         * Mathe, 5 Klasse Herr Diszberger ;)
          */
-        std::string romanLetters[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int integerValues[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        const std::string romanLetters[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        const int integerValues[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         std::string result = "";
 
+        /**
+         * Die 13 ist hier hart hinterlegt, ein romanLetters.size() würde nur unnötig 
+         * Rechenzeit benötigen. Es ist bekannt und die Länge ist fix, nicht dynamisch.
+         * 
+         */
         for (int i = 0; i < 13; ++i) {
             while(integerValueToConvert - integerValues[i] >= 0) {
                 result += romanLetters[i];
@@ -22,7 +29,7 @@ using namespace std;
     }
 
 int main() {  
-    int integerValueToConvert = 3999;
+    const int integerValueToConvert = 2999;
 	std::cout << "Wert als Integer " << integerValueToConvert << " : Als römische Zíffer " << convertIntToRoman(integerValueToConvert) << std::endl;
     return 0;  
 }
